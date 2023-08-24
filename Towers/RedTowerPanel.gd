f705c7767f423f9b78a400cfce3ca66e3805a112
Jade1724 <haruka.i1724@gmail.com>
2023-08-24 21:55:22 +1200

    Drag tower from the shop UI

--- Towers/RedTowerPanel.gd
+++ Towers/RedTowerPanel.gd
@@ -1,11 +1,19 @@
 extends Panel
 
+@onready var tower = preload("res://Towers/RedBulletTower.tscn")
+var curr_tile
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_gui_input(event):
+	var temp_tower = tower.instantiate()
+	if event is InputEventMouseButton and event.button_mask == 1:
+		#left click down
+		add_child(temp_tower)
+		temp_tower.process_mode = Node.PROCESS_MODE_DISABLED
+	elif event is InputEventMouseMotion and event.button_mask == 1:
+		# left click down drag
+		get_child(1).global_position = event.global_position
+		
+	elif event is InputEventMouseButton and event.button_mask == 0:
+		# left click up
+		print("left button up")
+		
