4333f03ef2c16894dfdc17d26570a9f798404e43
Jade1724 <haruka.i1724@gmail.com>
2023-08-25 21:51:21 +1200

    Drag and dropped the tower from shop

--- Towers/RedTowerPanel.gd
+++ Towers/RedTowerPanel.gd
@@ -6,14 +6,19 @@ var curr_tile
 func _on_gui_input(event):
 	var temp_tower = tower.instantiate()
 	if event is InputEventMouseButton and event.button_mask == 1:
+		print(event.global_position)
 		#left click down
 		add_child(temp_tower)
 		temp_tower.process_mode = Node.PROCESS_MODE_DISABLED
+		temp_tower.scale = Vector2(0.32, 0.32)
 	elif event is InputEventMouseMotion and event.button_mask == 1:
 		# left click down drag
 		get_child(1).global_position = event.global_position
-		
+				
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		# left click up
-		print("left button up")
-		
+		get_child(1).queue_free()
+		var path = get_tree().get_root().get_node("Main/Towers")
+		path.add_child(temp_tower)
+		temp_tower.global_position = event.global_position * 3
+		temp_tower.get_node("Area").hide()
