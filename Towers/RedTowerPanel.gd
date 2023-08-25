1bfe5d4dc3a12d5aab0770d699f8b447b4c91627
Jade1724 <haruka.i1724@gmail.com>
2023-08-25 22:10:04 +1200

    Prevent dropping a tower on the shop UI

--- Towers/RedTowerPanel.gd
+++ Towers/RedTowerPanel.gd
@@ -6,7 +6,6 @@ var curr_tile
 func _on_gui_input(event):
 	var temp_tower = tower.instantiate()
 	if event is InputEventMouseButton and event.button_mask == 1:
-		print(event.global_position)
 		#left click down
 		add_child(temp_tower)
 		temp_tower.process_mode = Node.PROCESS_MODE_DISABLED
@@ -19,11 +18,12 @@ func _on_gui_input(event):
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		# left click up
 		if get_child_count() > 1:
-			get_child(1).queue_free()
-		var path = get_tree().get_root().get_node("Main/Towers")
-		path.add_child(temp_tower)
-		temp_tower.global_position = event.global_position * 3
-		temp_tower.get_node("Area").hide()
+				get_child(1).queue_free()
+		if event.global_position.x * 3 <= 2944:
+			var path = get_tree().get_root().get_node("Main/Towers")
+			path.add_child(temp_tower)
+			temp_tower.global_position = event.global_position * 3
+			temp_tower.get_node("Area").hide()
 	else:
 		if get_child_count() > 1:
 			get_child(1).queue_free()
