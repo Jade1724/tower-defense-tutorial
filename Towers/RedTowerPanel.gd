2c285b8bfda5a7f13cfb33e14de65fe1d7a4fb8a
Jade1724 <haruka.i1724@gmail.com>
2023-08-25 21:58:57 +1200

    Prevented buying multiple towers at the same time

--- Towers/RedTowerPanel.gd
+++ Towers/RedTowerPanel.gd
@@ -13,12 +13,17 @@ func _on_gui_input(event):
 		temp_tower.scale = Vector2(0.32, 0.32)
 	elif event is InputEventMouseMotion and event.button_mask == 1:
 		# left click down drag
-		get_child(1).global_position = event.global_position
+		if get_child_count() > 1:
+			get_child(1).global_position = event.global_position
 				
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		# left click up
-		get_child(1).queue_free()
+		if get_child_count() > 1:
+			get_child(1).queue_free()
 		var path = get_tree().get_root().get_node("Main/Towers")
 		path.add_child(temp_tower)
 		temp_tower.global_position = event.global_position * 3
 		temp_tower.get_node("Area").hide()
+	else:
+		if get_child_count() > 1:
+			get_child(1).queue_free()
